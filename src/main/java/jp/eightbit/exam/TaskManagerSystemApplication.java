c0ead2e019fee9d9d64ba69c81f60cdc9df2c9f9
ryu-eb <r_donoshita@eight-bit.jp>
2024-09-17 12:10:15 +0900

    warファイル「作成用の「変更

--- src/main/java/jp/eightbit/exam/TaskManagerSystemApplication.java
+++ src/main/java/jp/eightbit/exam/TaskManagerSystemApplication.java
@@ -2,10 +2,17 @@ package jp.eightbit.exam;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TaskManagerSystemApplication {
+public class TaskManagerSystemApplication extends SpringBootServletInitializer{
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+			return application.sources(TaskManagerSystemApplication.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(TaskManagerSystemApplication.class, args);
 	}
